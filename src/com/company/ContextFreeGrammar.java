1cf9ee02fa0f5b4d35a334125588b66b2c57cbe7
Eddy De Leon <deleoneddy45@gmail.com>
2018-11-16 16:28:42 -0800

    Creating a method to generate productions after non-terminals have been declared.

--- src/com/company/ContextFreeGrammar.java
+++ src/com/company/ContextFreeGrammar.java
@@ -2,16 +2,24 @@ package com.company;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 
 public class ContextFreeGrammar {
     private ArrayList<Character> nonTerminalSymbols;
     private ArrayList<Character> terminalSymbols;
     private ArrayList<Map<Character, ArrayList<String>>> productions;
 
-    public ContextFreeGrammar(ArrayList<Character> nonTerminalSymbols, ArrayList<Character> terminalSymbols) {
+    public ContextFreeGrammar(ArrayList<Character> nonTerminalSymbols) {
         this.nonTerminalSymbols = nonTerminalSymbols;
-        this.terminalSymbols = terminalSymbols;
-        
+
+    }
+
+    public void generateProductions(ArrayList<Character> nonTerminalSymbols) {
+        System.out.println("Generate the productions for your non-terminal symbols.");
+        for (Character nonTerminal : nonTerminalSymbols) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("What are the productions for: " + nonTerminal + " -> ");
+        }
     }
 
 }
