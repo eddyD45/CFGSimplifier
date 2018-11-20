79411f9b0b6003c2f8e1e61a40b29319994df5f1
Eddy De Leon <deleoneddy45@gmail.com>
2018-11-20 11:55:32 -0800

    Created boolean methods that check if a given CFG has lambdas/variable productions/useless productions. Will be used to check when I need to remove something.

--- src/com/company/ContextFreeGrammar.java
+++ src/com/company/ContextFreeGrammar.java
@@ -5,21 +5,56 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class ContextFreeGrammar {
-    private ArrayList<Character> nonTerminalSymbols;
-    private ArrayList<Character> terminalSymbols;
-    private ArrayList<Map<Character, ArrayList<String>>> productions;
+    private Map<Character, ArrayList<String>> productions;
 
-    public ContextFreeGrammar(ArrayList<Character> nonTerminalSymbols) {
-        this.nonTerminalSymbols = nonTerminalSymbols;
-
+    public ContextFreeGrammar(Map<Character, ArrayList<String>> productions) {
+        this.productions = productions;
     }
 
-    public void generateProductions(ArrayList<Character> nonTerminalSymbols) {
-        System.out.println("Generate the productions for your non-terminal symbols.");
-        for (Character nonTerminal : nonTerminalSymbols) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("What are the productions for: " + nonTerminal + " -> ");
+    public boolean hasLambdaProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
+        for (String production : mapEntry.getValue()) {
+            if (production.equals("0")) {
+                System.out.println("Found lambda I must remove.");
+                return true;
+            }
         }
+        return false;
+    }
+
+    public boolean isVariableProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
+        ArrayList<String> productions = mapEntry.getValue();
+        for (String production : productions) {
+            if (production.length() == 1 && production.matches("[A-Z]")) {
+                System.out.println("Found variable production I must remove.");
+                return true;
+            }
+        }
+        return false;
+    }
+
+/*
+    The idea is: Iterate through all strings in the production of a map entry. Suppose, to begin, your production has no
+    non-terminal (considered some upper case character). Check each character in your string. If a character is uppercase,
+    set hasNonTerminal to true and end the iteration of the string there. After iterating through a string, check if has
+    a non terminal. If it does, do nothing and check the next string. If it does not have a non terminal, return the statement
+    with false, meaning this is non a useless production. If it goes through the entire list of strings and finds a non-terminal
+    in each string, then the statement returns true; it is a useless statement and must be removed.
+ */
+    public boolean isUselessProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
+        for (String production : mapEntry.getValue()) {
+            boolean hasNonTerminal = false;
+            for (int i = 0; i < production.length(); i++) {
+                if (Character.isUpperCase(production.charAt(i))) {
+                    hasNonTerminal = true;
+                    break;
+                }
+            }
+            if (!hasNonTerminal) {
+                return false;
+            }
+        }
+        System.out.println("Non terminals found in all strings. Found useless production I must remove.");
+        return true;
     }
 
 }
