63524166f7905dd3c7b6cb835ebf393bf4415d1d
Eddy De Leon <deleoneddy45@gmail.com>
2018-11-27 20:05:15 -0800

    Worked on lambda reduction.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //grammar from Exercise 7
+        Map<Character, ArrayList<String>> excercise7Grammar = new HashMap<>()
+        String [] productions1 = {"aS", "AB", "0"};
+        ArrayList<String> productionsList = new ArrayList<>(Arrays.asList(productions1));
+        excercise7Grammar.put('S', productionsList);
+
+        String [] productions2 = {"aB", "0"};
+
+
+
     }
 }
