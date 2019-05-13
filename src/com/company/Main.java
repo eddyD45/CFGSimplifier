6cd742648f2a6c814d304c7f033391327a75cda4
eddyD45 <deleoneddy45@gmail.com>
2019-05-12 20:48:12 -0700

    Fixed (?) useless production removal. At least works for Zuo's requirements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,77 +1,92 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //grammar from Exercise 7
-        Map<Character, ArrayList<String>> excercise7Grammar = new HashMap<>();
-        excercise7Grammar.put('S', new ArrayList<>(Arrays.asList("aS", "AB", "0")));
-        excercise7Grammar.put('A', new ArrayList<>(Arrays.asList("aB", "0")));
-        excercise7Grammar.put('B', new ArrayList<>(Arrays.asList("Aa")));
-        excercise7Grammar.put('C', new ArrayList<>(Arrays.asList("cCD")));
-        excercise7Grammar.put('D', new ArrayList<>(Arrays.asList("ddd", "Cd")));
+        Map<Character, ArrayList<String>> userCFG = getCFGFromUser();
 
-        //grammar from Exercise 10
-        Map<Character, ArrayList<String>> exercise10Grammar = new HashMap<>();
-        exercise10Grammar.put('S', new ArrayList<>(Arrays.asList("aA", "aBB")));
-        exercise10Grammar.put('A', new ArrayList<>(Arrays.asList("aaA", "0")));
-        exercise10Grammar.put('B', new ArrayList<>(Arrays.asList("bB", "bbC")));
-        exercise10Grammar.put('C', new ArrayList<>(Arrays.asList("B")));
 
-        System.out.println("Original grammar from exercise 7: " + excercise7Grammar.toString());
+//        //grammar from the example
+//        Map<Character, ArrayList<String>> exampleCFG = new HashMap<>();
+//        exampleCFG.put('S', new ArrayList<>(Arrays.asList("aA", "aBB")));
+//        exampleCFG.put('A', new ArrayList<>(Arrays.asList("aaA", "0")));
+//        exampleCFG.put('B', new ArrayList<>(Arrays.asList("bB", "bbC")));
+//        exampleCFG.put('C', new ArrayList<>(Arrays.asList("B")));
+//
+//        System.out.println("\nOriginal grammar from the example: " + exampleCFG.toString());
 
-        System.out.println("REMOVE LAMBDAS");
-        Map<Character, ArrayList<String>> exercise7GrammarWithoutLambda = new HashMap<>(removeLambdaProductions(excercise7Grammar));
-        System.out.println("Before: " + excercise7Grammar.toString());
-        System.out.println("After: " + exercise7GrammarWithoutLambda.toString());
+//        System.out.println("REMOVE EPSILON");
+//        Map<Character, ArrayList<String>> exampleCFGNoEpsilon = new HashMap<>(removeEpsilonProductions(exampleCFG));
+//        System.out.println("Before: " + exampleCFG.toString());
+//        System.out.println("After: " + exampleCFGNoEpsilon.toString());
+//
+//        System.out.println("\nREMOVE USELESS PRODUCTIONS");
+//
+//        Map<Character, ArrayList<String>> exampleSimplified = new HashMap<>(removeUselessProductions(exampleCFGNoEpsilon));
+//        System.out.println("Before: " + exampleCFG);
+//        System.out.println("After: " + exampleSimplified);
 
-        System.out.println("\nREMOVE VARIABLES");
+        System.out.println("\nOriginal grammar from user: " + userCFG.toString());
 
-        Map<Character, ArrayList<String>> exercise7GrammarWithoutLambdaOrVariables = new HashMap<>(removeUnitProductions(exercise7GrammarWithoutLambda));
-        System.out.println("Before: " + exercise7GrammarWithoutLambda);
-        System.out.println("After: " + exercise7GrammarWithoutLambdaOrVariables);
-
-        System.out.println("\nREMOVE USELESS PRODUCTIONS");
-
-        Map<Character, ArrayList<String>> exercise7Simplified = new HashMap<>(removeUselessProductions(exercise7GrammarWithoutLambdaOrVariables));
-        System.out.println("Before: " + exercise7GrammarWithoutLambdaOrVariables);
-        System.out.println("After: " + exercise7Simplified);
-
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-        System.out.println("\nOriginal grammar from exercise 10: " + exercise10Grammar.toString());
-
-        System.out.println("REMOVE LAMBDAS");
-        Map<Character, ArrayList<String>> exercise10GrammarWithoutLambda = new HashMap<>(removeLambdaProductions(exercise10Grammar));
-        System.out.println("Before: " + exercise10Grammar.toString());
-        System.out.println("After: " + exercise10GrammarWithoutLambda.toString());
-
-        System.out.println("\nREMOVE VARIABLES");
-
-        Map<Character, ArrayList<String>> exercise10GrammarWithoutLambdaOrVariables = new HashMap<>(removeUnitProductions(exercise10GrammarWithoutLambda));
-        System.out.println("Before: " + exercise10GrammarWithoutLambda);
-        System.out.println("After: " + exercise10GrammarWithoutLambdaOrVariables);
+        System.out.println("REMOVE EPSILON");
+        Map<Character, ArrayList<String>> userCFGNoEpsilon = new HashMap<>(removeEpsilonProductions(userCFG));
+        System.out.println("Before: " + userCFG.toString());
+        System.out.println("After: " + userCFGNoEpsilon.toString());
 
         System.out.println("\nREMOVE USELESS PRODUCTIONS");
 
-        Map<Character, ArrayList<String>> exercise10Simplified = new HashMap<>(removeUselessProductions(exercise10GrammarWithoutLambdaOrVariables));
-        System.out.println("Before: " + exercise10GrammarWithoutLambdaOrVariables);
-        System.out.println("After: " + exercise10Simplified);
+        Map<Character, ArrayList<String>> userCFGSimplified = new HashMap<>(removeUselessProductions(userCFGNoEpsilon));
+        System.out.println("Before: " + userCFGNoEpsilon);
+        System.out.println("After: " + userCFGSimplified);
 
 
     }
 
+    private static Map<Character, ArrayList<String>> getCFGFromUser() {
+        Map<Character, ArrayList<String>> userCFG = new HashMap<>();
+        Character userVariable = 'S';
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> instructions = getVariableInstructionsFromUser(userVariable);
+        userCFG.put(userVariable, instructions);
+
+        while (userVariable != '/') {
+            System.out.println("Enter another variable (enter '/' when done):");
+            userVariable = scanner.nextLine().toCharArray()[0];
+            if (userVariable != '/') {
+                instructions = getVariableInstructionsFromUser(userVariable);
+                userCFG.put(userVariable, instructions);
+            }
+        }
+
+        return userCFG;
+    }
+
+    private static ArrayList<String> getVariableInstructionsFromUser(char variable) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> instructions = new ArrayList<>();
+        String instructionString = "";
+        while (!(instructionString.equals("/"))) {
+            System.out.println("Enter instructions for variable " + variable + " (enter '/' when done):");
+            instructionString = scanner.nextLine();
+            if (!(instructionString.equals("/"))) {
+                instructions.add(instructionString);
+            }
+        }
+        return instructions;
+    }
+
+
     /*
     @param a Map Entry from a CFG
-    @return true if lambda production, denoted as a string "0", is found in the value of the entry,
+    @return true if epsilon production, denoted as a string "0", is found in the value of the entry,
     false if string "0" not found
      */
-    public static boolean hasLambdaProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
+    private static boolean hasEpsilonProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
         for (String production : mapEntry.getValue()) {
             if (production.equals("0")) {
                 return true;
@@ -79,22 +94,6 @@ public class Main {
         }
         return false;
     }
-
-    /*
-    @param a Map Entry from a CFG
-    @return true if a string in the list of productions is an uppercase character, denoting a variable,
-    false otherwise
-     */
-    public static boolean isVariableProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
-        ArrayList<String> productions = mapEntry.getValue();
-        for (String production : productions) {
-            if (production.length() == 1 && production.matches("[A-Z]")) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     /*
         The idea is: Iterate through all strings in the production of a map entry. Suppose, to begin, your production has no
         non-terminal (considered some upper case character). Check each character in your string. If a character is uppercase,
@@ -107,7 +106,7 @@ public class Main {
         @return true if all strings in the productions have at least one uppercase character, denoting a variable,
         false if at least one string in the productions are all lowercase, denoting a production of only terminals
      */
-    public static boolean isUselessProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
+    private static boolean isUselessProduction(Map.Entry<Character, ArrayList<String>> mapEntry) {
         for (String production : mapEntry.getValue()) {
             boolean hasNonTerminal = false;
             for (int i = 0; i < production.length(); i++) {
@@ -128,87 +127,44 @@ public class Main {
     @return new Map of the CFG with lambdas replaced.
      */
 
-    public static Map<Character, ArrayList<String>> removeLambdaProductions(Map<Character, ArrayList<String>> productions) {
+    private static Map<Character, ArrayList<String>> removeEpsilonProductions(Map<Character, ArrayList<String>> productions) {
         Map<Character, ArrayList<String>> newCFG = new HashMap<>();
-        ArrayList<Character> lambdaEntries = new ArrayList<>();
+        ArrayList<Character> epsilonEntries = new ArrayList<>();
 
-        //check all productions. if any of them have a lambda, add their keys to lambdaEntries to check
-        //add them to a new ArrayList to create a new grammar without lambdas, to be used in conjunction with the list
-        //of variables we've collected that have lambdas, so we can evaluate a given production with a variable as if it
+        //check all productions. if any of them have a epsilon, add their keys to epsilonEntries to check
+        //add them to a new ArrayList to create a new grammar without epsilons, to be used in conjunction with the list
+        //of variables we've collected that have epsilons, so we can evaluate a given production with a variable as if it
         //wasn't there
         for (Map.Entry<Character, ArrayList<String>> entry : productions.entrySet()) {
-            ArrayList<String> nonLambdaProductions = new ArrayList<>();
-            if (hasLambdaProduction(entry)) {
-                lambdaEntries.add(entry.getKey());
+            ArrayList<String> nonEpsilonProductions = new ArrayList<>();
+            if (hasEpsilonProduction(entry)) {
+                epsilonEntries.add(entry.getKey());
             }
             for (String production : entry.getValue()) {
                 if (entry.getKey() == 'S') {
-                    nonLambdaProductions.add(production);
+                    nonEpsilonProductions.add(production);
                 } else if (!production.equals("0")) {
-                    nonLambdaProductions.add(production);
+                    nonEpsilonProductions.add(production);
                 }
             }
-            newCFG.put(entry.getKey(), nonLambdaProductions);
+            newCFG.put(entry.getKey(), nonEpsilonProductions);
         }
 
-//        go through all productions and replace production strings that have a characterWithLambda with a blank and add
-//        it to to a new set of productions. Also keep the original. Ignore lambdas. Push a new entry to a new CFG map with the same key
-//        and the new productions (a combo of the old productions and the new productions with lambdas removed.
-        for (Character characterWithLambda : lambdaEntries) {
+//        go through all productions and replace production strings that have a characterWithEpsilon with a blank and add
+//        it to to a new set of productions. Also keep the original. Ignore epsilon. Push a new entry to a new CFG map with the same key
+//        and the new productions (a combo of the old productions and the new productions with epsilon removed.
+        for (Character characterWithEpsilon : epsilonEntries) {
             for (Map.Entry<Character, ArrayList<String>> entry : newCFG.entrySet()) {
                 ArrayList<String> newProductions = new ArrayList<>();
                 for (String production : entry.getValue()) {
                     newProductions.add(production);
-                    if (production.contains(characterWithLambda.toString())) {
-                        newProductions.add(production.replace(characterWithLambda.toString(), ""));
+                    if (production.contains(characterWithEpsilon.toString())) {
+                        newProductions.add(production.replace(characterWithEpsilon.toString(), ""));
                     }
                 }
                 newCFG.put(entry.getKey(), newProductions);
             }
         }
-
-        return newCFG;
-    }
-
-    /*
-    Find productions that have only one character, and check if they are an upper case Character, indicating they are
-    unit productions. Add that to a map that holds a Character, the key, and a String, the production. We will swap these
-    out later. After finding what keys we're getting rid of, go through and remove them as long as they're not start terminal S.
-
-     */
-    public static Map<Character, ArrayList<String>> removeUnitProductions(Map<Character, ArrayList<String>> productions) {
-        Map<Character, ArrayList<String>> newCFG = new HashMap<>(productions);
-        Map<Character, String> variablesToRemoveFromCFG = new HashMap<>();
-        for (Map.Entry<Character, ArrayList<String>> entry : newCFG.entrySet()) {
-            if (isVariableProduction(entry)) {
-                for (String production : entry.getValue()) {
-                    if (production.length() == 1 && production.matches("[A-Z]")) {
-                        variablesToRemoveFromCFG.put(entry.getKey(), production);
-                    }
-                }
-            }
-        }
-
-        for (Map.Entry<Character, String> entry : variablesToRemoveFromCFG.entrySet()) {
-            if (entry.getKey() != 'S') {
-                newCFG.remove(entry.getKey());
-            }
-        }
-
-        for (Map.Entry<Character, String> entryToRemove : variablesToRemoveFromCFG.entrySet()) {
-            for (Map.Entry<Character, ArrayList<String>> entry : newCFG.entrySet()) {
-                ArrayList<String> newProductions = new ArrayList<>();
-                for (String production : entry.getValue()) {
-                    if (entry.getKey() != 'S') {
-                        newProductions.add(production.replace(entryToRemove.getKey().toString(), entryToRemove.getValue()));
-                    } else {
-                        newProductions.add(production);
-                    }
-                }
-                newCFG.put(entry.getKey(), newProductions);
-            }
-        }
-
 
         return newCFG;
     }
@@ -217,14 +173,32 @@ public class Main {
     Go through map entries. If the productions of a given entry do not have a string with lower case characters only, it
     is considered useless and removed from the CFG.
      */
-    public static Map<Character, ArrayList<String>> removeUselessProductions(Map<Character, ArrayList<String>> productions) {
+    private static Map<Character, ArrayList<String>> removeUselessProductions(Map<Character, ArrayList<String>> productions) {
         Map<Character, ArrayList<String>> newCFG = new HashMap<>(productions);
+        ArrayList<Character> removedVariables = new ArrayList<>();
 
         for (Map.Entry<Character, ArrayList<String>> entry : productions.entrySet()) {
             if (isUselessProduction(entry)) {
+                removedVariables.add(entry.getKey());
                 newCFG.remove(entry.getKey());
             }
         }
-        return newCFG;
+
+        Map<Character, ArrayList<String>> newCFG2 = new HashMap<>(newCFG);
+        for (Map.Entry<Character, ArrayList<String>> entry : newCFG.entrySet()) {
+            ArrayList<String> entryInstructionList = new ArrayList<>(entry.getValue());
+            for (String instruction :
+                    entry.getValue()) {
+                for (Character removedVariable :
+                        removedVariables) {
+                    //if the string contains the removed variable, remove that instruction
+                    if (instruction.indexOf(removedVariable) != -1) {
+                        entryInstructionList.remove(instruction);
+                    }
+                }
+            }
+            newCFG2.put(entry.getKey(), entryInstructionList);
+        }
+        return newCFG2;
     }
 }
